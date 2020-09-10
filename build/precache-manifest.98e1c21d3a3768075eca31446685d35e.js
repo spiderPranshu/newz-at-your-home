8de36b1ac1c02cbaf3fcccb95ab38718a585e0e6
spiderPranshu <62246725+spiderPranshu@users.noreply.github.com>
2020-09-10 14:30:14 +0530

    Add files via upload

---
+++ build/precache-manifest.98e1c21d3a3768075eca31446685d35e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72bf9033ed81cae2475dc6f970d61b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbaee8da08c70725e154",
+    "url": "/static/css/main.e23e6b48.chunk.css"
+  },
+  {
+    "revision": "9d7b319e14fac7d58e10",
+    "url": "/static/js/2.63f6e2cb.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.63f6e2cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbaee8da08c70725e154",
+    "url": "/static/js/main.b1956656.chunk.js"
+  },
+  {
+    "revision": "db405a886d1dde86d8f6",
+    "url": "/static/js/runtime-main.ab6f4945.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+import logo from './images/images.jpg';
+import { NewsCards, Modal } from './components';
+import useStyles from './styles';
+
+const App = () => {
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: '551d51a5178276122994baf1aa29a9fb2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'instructions') {
+          setIsOpen(true);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+      {!newsArticles.length ? (
+        <div className={classes.footer}>
+          <Typography variant="body1" component="h2">
+            Created by
+            <a className={classes.link} href="https://whjr-v2-prod-bucket.s3.ap-south-1.amazonaws.com/a5825e55-e6ae-4d07-a522-5dd6c7f861e9.pdf"> Pranshu Singh</a> -
+            <a className={classes.link} href="https://whjr-v2-prod-bucket.s3.ap-south-1.amazonaws.com/1ae89fe7-a157-4120-b503-629a64fc46c8.pdf"> Game Developer</a>
+          </Typography>
+          <img className={classes.image} src={logo} height="50px" alt="Naruto" />
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default App;
